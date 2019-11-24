5b3c2167be7c7338e4a96487c1073815cc5cc347
Kanawha.st <tomoiida@gmail.com>
2019-11-24 14:17:18 +0900

    link to open new tab

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -7,7 +7,7 @@
     >
       <v-card-title class="mb-1 .display-2 .font-weight-bold">{{ title }}</v-card-title>
       <v-card-subtitle>
-        窓口: <a href="url" class="headline mb-1">{{ servicer }}</a>
+        窓口: <a v-bind:href="url" class="headline mb-1" target="_blank" rel="noopener noreferrer" >{{ servicer }}</a>
         <span v-if="TBD" class="ml-5 red--text text--lighten-1">※{{ TBD }}</span>
       </v-card-subtitle>
       <v-card-text>{{ description }}</v-card-text>
